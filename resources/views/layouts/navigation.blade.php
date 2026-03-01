2053a05f889c0a52dfba820fe982794c813b4431
ahmed elbardawell <brdweelahmed@gmail.com>
2026-03-02 01:32:07 +0200

    Show Admin Dashboard link in navigation for admins

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,11 @@
                     <x-nav-link :href="route('wallet.index')" :active="request()->routeIs('wallet.*')">
                         {{ __('Wallet') }}
                     </x-nav-link>
+                    @if(Auth::user()->is_admin)
+                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
+                        {{ __('Admin Dashboard') }}
+                    </x-nav-link>
+                    @endif
                 </div>
             </div>
 
@@ -76,6 +81,11 @@
             <x-responsive-nav-link :href="route('wallet.index')" :active="request()->routeIs('wallet.*')">
                 {{ __('Wallet') }}
             </x-responsive-nav-link>
+            @if(Auth::user()->is_admin)
+            <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
+                {{ __('Admin Dashboard') }}
+            </x-responsive-nav-link>
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
